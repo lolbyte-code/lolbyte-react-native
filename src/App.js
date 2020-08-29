6033ca1c682bbee812a1cfd32b8ca628b24cdd57
Anthony Schott <acschott526@gmail.com>
2020-08-29 15:59:37 -0700

    Write/read Redux state to/from local storage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {getFromStorage, setToStorage} from './utils/Storage';
+
 import Home from './components/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import Profile from './components/Profile';
@@ -5,22 +7,26 @@ import {Provider} from 'react-redux';
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createStore} from 'redux';
-import {getFromStorage} from './utils/Storage';
 import summonersReducer from './reducers/SummonersReducer';
 
 const Stack = createStackNavigator();
 
-let summonersStore = createStore(summonersReducer);
-// getFromStorage('@summoners').then((summoners) => {
-//   if (summoners == null) {
-//     return;
-//   }
-//   summonersStore = createStore(summonersReducer, summoners);
-// });
-
 const LolByteApp = () => {
+  const [store, setStore] = React.useState(createStore(summonersReducer));
+  store.subscribe(() => {
+    setToStorage('@summoners', store.getState());
+  });
+
+  React.useEffect(() => {
+    getFromStorage('@summoners').then((summoners) => {
+      if (summoners !== null) {
+        setStore(createStore(summonersReducer, summoners));
+      }
+    });
+  }, []);
+
   return (
-    <Provider store={summonersStore}>
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
